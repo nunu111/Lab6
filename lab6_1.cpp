9260c41a4787930172749d768f4ad1a7710acc67
nunu111 <nunu111za@hotmail.com>
2021-11-26 15:55:01 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
 using namespace std;
 
+int odd_even,odd,even;
+
+int Number (){
+    cin >> odd_even;
+    while(odd_even != 0){
+        if (odd_even%2 == 0){
+            even++;
+            cout << "Enter an integer: ";
+            Number();
+        }
+        else{
+            odd++;
+            cout << "Enter an integer: ";
+            Number();
+        }
+    }
+    return 0;
+}
+
 int main(){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    Number();
+    cout << "#Even numbers = "<< even <<"\n";
+    cout << "#Odd numbers = "<< odd;
     return 0;
 }
